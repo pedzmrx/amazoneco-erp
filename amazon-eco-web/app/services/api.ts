08689df1ae9d095853f270056173c207cbcabbf7
Pedro Moraes <pedromoraes750@gmail.com>
2026-08-29 19:56:28 +0000

    fix: corrige fluxo de autenticacao, estabiliza rotas de login e remove loop de redirect

--- amazon-eco-web/app/services/api.ts
+++ amazon-eco-web/app/services/api.ts
@@ -1,18 +1,3 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'https://cautious-carnival-975r67g755x62977v-3333.app.github.dev',
-});
-
-api.interceptors.request.use((config) => {
-  if (typeof window !== 'undefined') {
-    const token = localStorage.getItem('@AmazonEco:token');
-    
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+import { api } from '../../services/api';
+export * from '../../services/api';
+export default api;
---
+++ amazon-eco-web/services/api.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'https://cautious-carnival-975r67g755x62977v-3333.app.github.dev',
+});
+
+api.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('@AmazonEco:token');
+    
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
+
